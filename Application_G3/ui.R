286cddb47ee0328d4f50b0922261ad2c021c43d5
GloryFormbang <gndumchop@tu.isara.fr>
2026-02-10 17:25:45 +0100

    modifcations "couleur de texte"

--- Application_G3/ui.R
+++ Application_G3/ui.R
@@ -1,91 +1,89 @@
 library(shiny)
 library(leaflet)
+library(bslib)
 
-# Define UI for application that draws a histogram
+# Define UI for application
 fluidPage(
+  theme = bs_theme(
+    bg = "white",        # couleur de fond
+    fg = "blue",         # couleur du texte
+    primary = "#0d6efd", # couleur principale
+    secondary = "#6c757d", # couleur secondaire
+    base_font = font_google("Roboto")
+  ),
+  
   titlePanel("Carte des AOP Françaises"), 
   
   tabsetPanel(
     tabPanel("Accueil",
-             titlePanel(icon("home"),"Accueil"),
+             titlePanel(icon("home"), "Accueil"),
              
              passwordInput("password", "Password:"),
              actionButton("go", "Go"),
              verbatimTextOutput("value"), 
              
-             selectInput("variable", "Type d'AOP:",
-                         c("Cylinders" = "cyl",
-                           "Transmission" = "am",
-                           "Gears" = "gear")),
-             tableOutput("data_AOP")),
-    
-    selectInput("variable", "Nom du département:",
-                c("Cylinders" = "cyl",
-                  "Transmission" = "am",
-                  "Gears" = "gear")),
-    tableOutput("data_département")
-  ),
-  
-  imageOutput("aop_image.pjp"),
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-    )
-  ) # fermeture sidebarLayout
-  ,# fermeture tabPanel1
-  
-  tabPanel("Carte AOP",
-           titlePanel("Carte Interactive des AOP"),
-           fluidRow(column(width = 4),
-                    column(width = 2, offset = 3)),
-           fluidRow(column(width = 12)),
-           checkboxGroupInput("icons", "Choose icons:",
-                              choiceNames =
-                                list(icon("calendar"), icon("bed"),
-                                     icon("cog"), icon("bug")),
-                              choiceValues =
-                                list("calendar", "bed", "cog", "bug")
-           ),
-           sidebarLayout(
-             sidebarPanel(checkboxGroupInput("variable", "Variables to show:",
-                                             c("Cylinders" = "cyl",
-                                               "Transmission" = "am",
-                                               "Gears" = "gear"))
+             navlistPanel(
+               "Liste",
+               tabPanel("Catégorie produits"),
+               tabPanel("Nom de la région")
              ),
-             mainPanel(leafletOutput("map")
-             ) 
              
-           )), 
-  
-  tabPanel("About",
-           titlePanel("L'Equipe"),
-           
-           checkboxGroupInput("variable", "Variables to show:",
-                              c("Cylinders" = "cyl",
-                                "Transmission" = "am",
-                                "Gears" = "gear")),
-           fluidRow(column(width = 4),
-                    column(width = 2, offset = 3)),
-           fluidRow(column(width = 12)),
-           checkboxGroupInput("variable", "Variables to show:",
-                              c("Cylinders" = "cyl",
-                                "Transmission" = "am",
-                                "Gears" = "gear"))
-  )# fermeture tabsetPanel
-  
-) # fermeture Fluidpage
+             imageOutput("aop_image")
+    ), # fermeture tabPanel Accueil
+    
+    tabPanel("Carte AOP",
+             titlePanel("Carte Interactive des AOP"),
+             
+             selectInput("variable_produit", "Catégorie produits:",
+                         c("Boissons" = "cyl",
+                           "Produits carnés" = "am",
+                           "Produits laitiers" = "gear", 
+                           "Autres" = "autre")),
+             tableOutput("data_AOP"),
+             
+             selectInput("variable_region", "Nom de la région:",
+                         c("Auvergne rhone alpes" = "cyl",
+                           "Iles de France" = "am",
+                           "Occitanie" = "Occitanie")),
+             tableOutput("data_département"),
+             
+             fluidRow(
+               column(width = 4),
+               column(width = 2, offset = 3)
+             ),
+             fluidRow(column(width = 12)),
+             
+             checkboxGroupInput("icons", "Choose icons:",
+                                choiceNames = list(
+                                  icon("calendar"), 
+                                  icon("bed"),
+                                  icon("cog"), 
+                                  icon("bug")
+                                ),
+                                choiceValues = list(
+                                  "calendar", "bed", "cog", "bug"
+                                )
+             )
+    ), # fermeture tabPanel Carte AOP
+    
+    tabPanel("About",
+             titlePanel("L'Equipe"),
+             
+             checkboxGroupInput("variable_about", "Variables to show:",
+                                c("Cylinders" = "cyl",
+                                  "Transmission" = "am",
+                                  "Gears" = "gear")),
+             
+             fluidRow(
+               column(width = 4),
+               column(width = 2, offset = 3)
+             ),
+             fluidRow(column(width = 12)),
+             
+             checkboxGroupInput("variable_about2", "Variables to show:",
+                                c("Cylinders" = "cyl",
+                                  "Transmission" = "am",
+                                  "Gears" = "gear"))
+    ) # fermeture tabPanel About
+  ) # fermeture tabsetPanel
+) # fermeture fluidPage
